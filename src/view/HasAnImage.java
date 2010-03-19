4733fc674fc8fa3c032981cdac06ed22cffb6393
Scandinavianblue <Scandinavianblue@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-19 23:27:10 +0000

    Added View's abstract classes

---
+++ src/view/HasAnImage.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package src.view;
+
+/**
+ *
+ * @author spock
+ */
+public class HasAnImage {
+
+}
