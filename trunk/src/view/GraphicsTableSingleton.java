b68fbc67160d980be67c9a0f2015cfde6f9e840b
Scandinavianblue <Scandinavianblue@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 16:18:58 +0000

    added close Menu method

--- trunk/src/view/GraphicsTableSingleton.java
+++ trunk/src/view/GraphicsTableSingleton.java
@@ -28,29 +28,11 @@ class GraphicsTableSingleton {
         return graphics.get(key);
     }
     private static class GraphicsTableSingletonHolder {
-        private static final GraphicsTableSingleton INSTANCE =
+        private static final GraphicsTableSingleton ONEANDONLY =
                                             new GraphicsTableSingleton();
     }
 
     public static GraphicsTableSingleton getInstance() {
-        return GraphicsTableSingletonHolder.INSTANCE;
+        return GraphicsTableSingletonHolder.ONEANDONLY;
     }
-
 }
-/*
- public class Singleton {
-    //Private constructor prevents instantiation from other classes
-   private Singleton() {}
-
-    // SingletonHolder is loaded on the first execution of Singleton.getInstance()
-    // or the first access to SingletonHolder.INSTANCE, not before.
-
-   private static class SingletonHolder {
-     private static final Singleton INSTANCE = new Singleton();
-   }
-
-   public static Singleton getInstance() {
-     return SingletonHolder.INSTANCE;
-   }
- }
- */
