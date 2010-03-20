62fdf929812b7ea803da9b55d7685a28b211e61e
Scandinavianblue <Scandinavianblue@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 06:43:08 +0000

    Lots of View Skeleton code completed and SakuraMap interface added

---
+++ trunk/src/model/interfaces/SakuraMap.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package src.model.interfaces;
+
+import src.view.MapBuilder;
+
+/**
+ *
+ * @author spock
+ */
+public interface SakuraMap {
+    public int mapWidth();
+    public int mapHeight();
+    public void build(MapBuilder b);
+}
