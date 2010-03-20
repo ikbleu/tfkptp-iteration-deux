29359d5719c705edfbbdabb4b8fb4078364a605b
chris.woolfe <chris.woolfe@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 20:45:50 +0000

    added file and KeyMap related interfaces

---
+++ trunk/src/control/interfaces/EditableKeyConfig.java
@@ -0,0 +1,9 @@
+package src.control.interfaces;
+import src.control.*;
+
+public interface EditableKeyConfig {
+	public boolean bindMeaning(String context, String meaning, KeyCodeAndModifiers event);
+	public boolean unbindMeaning(String context, String meaning);
+	public boolean addContext(String context);
+	public boolean removeContext(String context);
+}
