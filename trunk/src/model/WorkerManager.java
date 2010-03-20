e14f9e33852b192130e7fb041b81958e668bb9d7
Luminaire1202 <Luminaire1202@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 23:18:48 +0000

    - Created WorkerGroupFactory interface. - Created WorkerManager, which is also a WorkerGroupFactory. - Created the different kinds of worker groups.

---
+++ trunk/src/model/WorkerManager.java
@@ -0,0 +1,136 @@
+/*
+ * file: WorkerManager.java
+ */
+
+package src.model;
+
+import src.model.interfaces.WorkerGroupFactory;
+import src.model.interfaces.GameTile;
+
+import src.model.instances.workergroups.BreedingGroup;
+import src.model.instances.workergroups.HarvestingGroup;
+import src.model.instances.workergroups.NormalWorkerGroup;
+
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * Manages and creates new groups of workers.
+ *
+ * @author Christopher Dudley
+ */
+public class WorkerManager implements WorkerGroupFactory
+{
+    private Player owner;
+
+    private Map<String, Integer> breedStats;
+    private Map<String, Integer> harvestStats;
+    private Map<String, Integer> normalStats;
+
+    // Total number of workers that can be in existance.
+    private static final int MAX_WORKERS = 100;
+
+    // Total number of workers currently in existance.
+    private int totalWorkers;
+
+    public WorkerManager(Player owner)
+    {
+        this.owner = owner;
+
+        totalWorkers = 0;
+
+        breedStats = new HashMap<String, Integer>();
+
+        breedStats.put("density", MAX_WORKERS);
+        breedStats.put("breedingRate", 100);
+
+        harvestStats = new HashMap<String, Integer>();
+
+        harvestStats.put("density", 1);
+        harvestStats.put("harvestRate", 1);
+
+        normalStats = new HashMap<String, Integer>();
+
+        normalStats.put("density", MAX_WORKERS);
+    }
+
+    /**
+     * Updates that a certain number of workers were added to a worker group
+     * being managed by this manager.
+     *
+     * @param numWorkers the number of workers added.
+     */
+    public void workersAdded(int numWorkers)
+    {
+        totalWorkers += numWorkers;
+    }
+
+    /**
+     * Updates that a certain number of workers were removed from a worker group
+     * being managed by this manager.
+     *
+     * @param numWorkers the number of workers removed.
+     */
+    public void workersRemoved(int numWorkers)
+    {
+        totalWorkers -= numWorkers;
+    }
+
+    /**
+     * Returns the number of workers that can still be created.
+     *
+     * @return the number of workers that can still be created.
+     */
+    public int numWorkersLeft()
+    {
+        return MAX_WORKERS - totalWorkers;
+    }
+
+    /**
+     * Returns the player that owns the worker groups managed by the manager.
+     *
+     * @return the owning player.
+     */
+    public Player player()
+    {
+        return owner;
+    }
+
+    /**
+     * Creates a new worker group that has the capability of breeding to create
+     * more workers.
+     *
+     * @param location the location at which the worker group is being created.
+     *
+     * @return the new BreedingGroup.
+     */
+    public BreedingGroup newBreedingGroup(GameTile location)
+    {
+        return new BreedingGroup(location, breedStats, this);
+    }
+
+    /**
+     * Creates a new worker group that has the capability of harvesting to
+     * gather more raw resources.
+     *
+     * @param location the location at which the worker group is being created.
+     *
+     * @return the new HarvestingGroup.
+     */
+    public HarvestingGroup newHarvestingGroup(GameTile location, String resourceType)
+    {
+        return new HarvestingGroup(location, harvestStats, this, resourceType);
+    }
+
+    /**
+     * Creates a new worker group that does not have any special capabilities.
+     *
+     * @param location the location at which the worker group is being created.
+     *
+     * @return the new NormalWorkerGroup.
+     */
+    public NormalWorkerGroup newNormalWorkerGroup(GameTile location)
+    {
+        return new NormalWorkerGroup(location, normalStats, this);
+    }
+}
--- trunk/src/model/instances/WorkerGroup.java
+++ trunk/src/model/instances/WorkerGroup.java
@@ -4,12 +4,14 @@
 
 package src.model.instances;
 
-import java.util.ArrayList;
+import src.model.Player;
 
 import src.model.interfaces.GameTile;
-import src.model.interfaces.MovementListener;
-import src.model.interfaces.RadiusListener;
 import src.model.interfaces.LocatableVisitor;
+
+import java.util.Map;
+
+import src.model.WorkerManager;
 
 /**
  * Manages groups of workers that are assigned to certain tasks.
@@ -21,13 +23,25 @@ public abstract class WorkerGroup extends Locatable
     // Number of workers in the group.
     private int numWorkers;
 
+    // Contains various stats shared among worker groups.
+    private Map<String, Integer> stats;
+
+    // This worker group's manager.
+    private WorkerManager manager;
+
     /**
      * Creates a new worker group with no workers in it.
+     *
+     * @param location whre the worker group is located.
+     * @param stats the stats this group shares with other groups.
      */
-    public WorkerGroup(GameTile location)
+    public WorkerGroup(GameTile location, Map<String, Integer> stats,
+            WorkerManager manager)
     {
         super(location);
 
+        this.stats = stats;
+        this.manager = manager;
         numWorkers = 0;
     }
 
@@ -52,28 +66,86 @@ public abstract class WorkerGroup extends Locatable
     public void transferWorkers(WorkerGroup target, int numToTransfer)
     {
         int numTransfering = Math.min(numToTransfer, numWorkers);
-        target.addWorkers(numTransfering);
+        int spaceLeft = target.stats.get("density") - target.numWorkers;
+        numTransfering = Math.min(spaceLeft, numTransfering);
+
         this.removeWorkers(numTransfering);
+        target.addWorkers(numTransfering);
     }
 
+    /**
+     * Adds the specified number of workers to the group.
+     *
+     * @param numAdded the number of workers to add.
+     */
     protected void addWorkers(int numAdded)
     {
         numWorkers += numAdded;
+        manager.workersAdded(numAdded);
     }
 
+    /**
+     * Removes the specified number of workers to the group.
+     *
+     * @param numRemoved the number of workers to remove.
+     */
     protected void removeWorkers(int numRemoved)
     {
         numWorkers -= numRemoved;
+        manager.workersRemoved(numRemoved);
     }
 
+    /**
+     * Returns the number of workers that can still be created.
+     *
+     * @return the number of workers that can still be created.
+     */
+    protected int numWorkersLeft()
+    {
+        return manager.numWorkersLeft();
+    }
+
+    /**
+     * Returns the stat associated with the specified token.
+     *
+     * @param token the token for the stat.
+     * @return the integer stat.
+     */
+    protected int getStat(String token)
+    {
+        return stats.get(token);
+    }
+
+    /**
+     * Returns the player that owns the worker group.
+     *
+     * @return the player that owns the worker group.
+     */
+    public Player player()
+    {
+        return manager.player();
+    }
+
+    /**
+     * Workers don't care when other objects are within their influence radius,
+     * so this does nothing.
+     *
+     * @param i the locatable object that has entered the worker's influence radius.
+     */
     public void entered( Locatable i )
     {
-        // Should never be called.
+        // Workers don't care what enters their influence radius
     }
 
+    /**
+     * Workers don't care when other objects are within their influence radius,
+     * so this does nothing.
+     *
+     * @param i the locatable object that has entered the worker's influence radius.
+     */
     public void exited( Locatable i )
     {
-        // Should never be called.
+        // Workers don't care about what exits their influence radius
     }
 
     public void accept( LocatableVisitor lv )
