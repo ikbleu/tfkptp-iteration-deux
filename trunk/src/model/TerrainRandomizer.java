b61ea708886760980087f29d579da87db5ea5adf
wiredshaiberu <wiredshaiberu@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 17:02:28 +0000

    Tweaks to GameMap, GameTile, and HexTile; removed unneeded enums, and added Randomizer and TerrainRandomizer.

---
+++ trunk/src/model/TerrainRandomizer.java
@@ -0,0 +1,56 @@
+package src.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import src.model.interfaces.Randomizer;
+
+public class TerrainRandomizer implements Randomizer
+{
+	private Map<String, Integer> map;
+	
+	public TerrainRandomizer()
+	{
+		map = new HashMap<String, Integer>();
+		
+		map.put("GRASSLAND", new Integer(50));
+		map.put("SPARSE_FOREST", new Integer(30));
+		map.put("WATER", new Integer(15));
+		map.put("OUTER_SPACE", new Integer(5));
+	}
+	
+	@Override
+	public String random()
+	{
+		int i = (int)(Math.random()*100);
+		
+		int tot = map.get("GRASSLAND").intValue();
+		
+		if (i < tot)
+			return "GRASSLAND";
+		
+		tot += map.get("SPARSE_FOREST").intValue();
+		
+		if (i < tot)
+			return "SPARSE_FOREST";
+		
+		tot += map.get("WATER").intValue();
+		
+		if (i < tot)
+			return "WATER";
+		
+		return "OUTER_SPACE";
+	}
+	
+	public String defaultValue()
+	{
+		return "GRASSLAND";
+	}
+	
+	public Set<String> possibleValues()
+	{
+		return map.keySet();
+	}
+
+}
