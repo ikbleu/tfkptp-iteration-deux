a90eab1fda50be233a18cce5938aca3cb998adfd
wiredshaiberu <wiredshaiberu@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 07:02:18 +0000

    Added the methods for the Map-specific spacial manager (the one dealing only with GameTiles).  Partially implemented.

--- src/model/MapSpacialManager.java
+++ src/model/MapSpacialManager.java
@@ -1,5 +1,39 @@
 package src.model;
 
-public class MapSpacialManager {
+import java.util.List;
 
+import src.model.enums.Direction;
+import src.model.interfaces.GameTile;
+
+public class MapSpacialManager
+{
+	public static List<GameTile> getTilesAround(GameTile tile, int radius)
+	{
+		return null;
+	}
+	
+	public static int getDistanceBetween(GameTile t1, GameTile t2)
+	{
+		return (Math.abs(t1.getX() - t2.getX())+ Math.abs(t1.getY() - t2.getY())+ Math.abs(t1.getZ() - t2.getZ()))/2;
+	}
+	
+	public static List<Direction> getDirections(GameTile t1, GameTile t2)
+	{
+		return null;
+	}
+	
+	public static List<GameTile> getTilesInDirection(GameTile t, Direction dir, int distance)
+	{
+		return null;
+	}
+	
+	public static GameTile getNeighborAt(GameTile t, Direction d)
+	{
+		return t.getNeighbor(d);
+	}
+	
+	public static boolean hasNeighborAt(GameTile t, Direction d)
+	{
+		return t.getNeighbor(d) != null;
+	}
 }
