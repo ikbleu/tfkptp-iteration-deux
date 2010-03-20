9c49431af21a537edd2c66a3947f1261987d4d8f
Scandinavianblue <Scandinavianblue@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 07:05:24 +0000

    Added new implemented visitors and tech tree class

---
+++ src/view/JackTheViewVisitor.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package src.view;
+
+import src.model.interfaces.ViewVisitor;
+import src.model.interfaces.vGroup;
+import src.model.interfaces.vType;
+import src.model.interfaces.vInstance;
+import src.model.interfaces.vCommand;
+import src.model.interfaces.vArgument;
+
+/**
+ *
+ * @author spock
+ */
+public class JackTheViewVisitor implements ViewVisitor{
+    public void visitGroup( vGroup v ){
+
+    }
+
+    public void visitType( vType v ){
+
+    }
+
+    public void visitInstance( vInstance v ){
+
+    }
+
+    public void visitCommand( vCommand v ){
+
+    }
+    
+    public void visitArgument( vArgument v ){
+
+    }
+}
