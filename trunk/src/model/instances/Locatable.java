6fca20fb37d87bd45748149bae52cc02f6dcc9b8
ross456 <ross456@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 05:55:42 +0000

    changed Locatable to interface

--- trunk/src/model/instances/Locatable.java
+++ trunk/src/model/instances/Locatable.java
@@ -4,11 +4,16 @@ import src.model.interfaces.GameTile;
 import src.model.interfaces.LocatableVisitor;
 import src.model.interfaces.RadiusListener;
 
-public interface Locatable {
-	GameTile location();
-	int influenceRadius();
-	void addRadiusListener( RadiusListener rl );
-	void accept( LocatableVisitor lv );
-	void instanceEntered( Instance i );
-	void instanceExited( Instance i );
+public abstract class Locatable {
+	public Locatable()
+	{
+		// TODO: register self with location manager
+	}
+	
+	abstract public GameTile location();
+	abstract public int influenceRadius();
+	abstract public void addRadiusListener( RadiusListener rl );
+	abstract public void accept( LocatableVisitor lv );
+	abstract public void instanceEntered( Instance i );
+	abstract public void instanceExited( Instance i );
 }
