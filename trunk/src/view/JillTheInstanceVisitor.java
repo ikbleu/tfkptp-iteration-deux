0d93b93164ad251adcdd5e1a82ce2ada96ed4371
Scandinavianblue <Scandinavianblue@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 07:05:24 +0000

    Added new implemented visitors and tech tree class

---
+++ trunk/src/view/JillTheInstanceVisitor.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package src.view;
+
+import src.model.interfaces.InstanceVisitor;
+import src.model.interfaces.vUnit;
+import src.model.interfaces.vStructure;
+import src.model.interfaces.vRallyPoint;
+
+
+/**
+ *
+ * @author spock
+ */
+public class JillTheInstanceVisitor implements InstanceVisitor{
+
+    
+
+    public void visitUnit( vUnit u ){
+
+    }
+
+    public void visitStructure( vStructure s ){
+
+    }
+
+    public void visitRallyPoint( vRallyPoint r ){
+
+    }
+
+}
