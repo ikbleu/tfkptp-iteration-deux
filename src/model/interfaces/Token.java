42fac62a71f5a2d3353eed229b6a0d5fdce2c679
Scandinavianblue <Scandinavianblue@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 03:00:28 +0000

    replaced for Tokens instead of TypeCode

---
+++ src/model/interfaces/Token.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package src.model.interfaces;
+
+/**
+ *
+ * @author spock
+ */
+public interface Token {
+    public abstract String token();
+}
