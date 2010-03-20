d367eea0e6ef3a57364a5c67aec461f84aee6e2c
Scandinavianblue <Scandinavianblue@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 22:07:58 +0000

    edited Bob and MapBuilder as well as Jill

--- trunk/src/view/MapBuilder.java
+++ trunk/src/view/MapBuilder.java
@@ -5,15 +5,15 @@
 
 package src.view;
 
-import src.model.interfaces.Displayable;
 import src.model.enums.DecalType;
 import src.model.enums.Visibility;
+import src.model.enums.Direction;
 /**
  *
  * @author spock
  */
 public interface MapBuilder {
-    public void setStructure(Displayable structure);
+    public void setStructure(String structure);
     public void setWorkers(int workers);
     public void setIdle(int idles);
     public void setBreeding(int breeders);
@@ -22,7 +22,8 @@ public interface MapBuilder {
     public void setResources(int[] resources);
     public void setItem(String item);
     public void setDecal(DecalType decal);
-    public void setRallyPoints(Displayable[] rallyPoints);
+    public void addRallyPoint(int rallyPoint, Direction d, String status);
     public void setVisibility(Visibility vis);
     public void setTerrain(String terrain);
+    public void setIndividualUnits(int individualUnits);
 }
