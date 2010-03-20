48ef024a1139bb853a27d13968385ff0924a94ca
ross456 <ross456@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 16:50:25 +0000

    made listening for commands take a token

--- src/model/instances/CommandSender.java
+++ src/model/instances/CommandSender.java
@@ -2,6 +2,6 @@ package src.model.instances;
 
 public interface CommandSender {
 	public void executeCommand( Command c );
-	public void addCommandListener( CommandListener cl );
-	public void removeCommandListener( CommandListener cl );
+	public void addCommandListener( String token, CommandListener cl );
+	public void removeCommandListener( String token, CommandListener cl );
 }
