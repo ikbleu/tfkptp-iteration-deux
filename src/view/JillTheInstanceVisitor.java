13f5aae5ab7861d6fed63d61c8d3e517423c4c85
Scandinavianblue <Scandinavianblue@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 19:23:15 +0000

    Jack Jill and Bob

--- src/view/JillTheInstanceVisitor.java
+++ src/view/JillTheInstanceVisitor.java
@@ -18,23 +18,23 @@ import java.util.Map;
  * @author spock
  */
  class JillTheInstanceVisitor implements InstanceVisitor{
-     String type = null;
-     int workers = -1;
-     int id = -1;
-     int health = -1;
-     Map<String, Integer> stats;
-     List<vUnit> rallypoint;
+ String instanceType = null;
+ int workers = -1;
+ int id = -1;
+ int health = -1;
+ Map<String, Integer> stats;
+ List<vUnit> rallypoint;
     
 
     public void visitUnit( vUnit u ){
-        type = u.token();
+        instanceType = u.token();
         stats = u.stats();
         id = u.id();
         health = u.health();
     }
 
     public void visitStructure( vStructure s ){
-        type = s.token();
+        instanceType = s.token();
         stats = s.stats();
         id = s.id();
         health = s.health();
@@ -42,7 +42,7 @@ import java.util.Map;
     }
 
     public void visitRallyPoint( vRallyPoint r ){
-        type = r.token();
+        instanceType = r.token();
         workers = r.workers();
         rallypoint = r.units();
     }
