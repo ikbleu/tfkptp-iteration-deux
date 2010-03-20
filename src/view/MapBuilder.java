e7947b5f2617a67e41e63e00184a3837cce8f4c8
Scandinavianblue <Scandinavianblue@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-20 08:33:23 +0000

    edited the MapBuilder interface

--- src/view/MapBuilder.java
+++ src/view/MapBuilder.java
@@ -5,10 +5,27 @@
 
 package src.view;
 
+import src.model.interfaces.Displayable;
+import src.model.enums.DecalType;
+import src.model.enums.TerrainType;
+import src.model.enums.Visibility;
+import src.model.enums.ItemType;
 /**
  *
  * @author spock
  */
 public interface MapBuilder {
+    public void setStructure(Displayable structure);
+    public void setWorkers(int workers);
+    public void setIdle(int idles);
+    public void setBreeding(int breeders);
+    public void setSoldiersInside(int soldiers);
+    public void setHarvesters(int[] harvesters);
+    public void setResources(int[] resources);
+    public void setItem(ItemType item);
+    public void setDecal(DecalType decal);
+    public void setRallyPoints(Displayable[] rallyPoints);
+    public void setVisibility(Visibility vis);
+    public void setTerrain(TerrainType terrain);
 
 }
