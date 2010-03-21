77ca9debb5bcdfa21947307187f48fad4652f5f5
wiredshaiberu <wiredshaiberu@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-21 06:16:28 +0000

    Added spacial methods to GameTile.

--- trunk/src/model/interfaces/GameTile.java
+++ trunk/src/model/interfaces/GameTile.java
@@ -1,5 +1,8 @@
 package src.model.interfaces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import src.model.enums.Direction;
 import src.model.TokenTerrainWalkability;
 
@@ -18,4 +21,11 @@ public interface GameTile {
 	public void unmark();
 	public String getTerrainType();
 	public boolean hasNeighbor(Direction d);
+	
+	// spacial methods
+	public List<GameTile> getTilesAround(int radius);
+	public int getDistanceFrom(GameTile t);
+	public List<Direction> getDirectionsTo(GameTile t);
+	public List<GameTile> getTilesInDirection(Direction dir, int distance);
 }
+
