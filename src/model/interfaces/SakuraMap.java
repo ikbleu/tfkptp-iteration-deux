385a501d816fe4ee70d29964305452874ba4e0a1
wiredshaiberu <wiredshaiberu@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-21 23:06:31 +0000

    Player Resources are now obtainable by the view yay.

--- src/model/interfaces/SakuraMap.java
+++ src/model/interfaces/SakuraMap.java
@@ -5,6 +5,8 @@
 
 package src.model.interfaces;
 
+import java.util.Map;
+
 import src.view.MapBuilder;
 
 /**
@@ -15,4 +17,5 @@ public interface SakuraMap {
     public int mapWidth();
     public int mapHeight();
     public void build(MapBuilder[][] b);
+    public Map<String, Integer> getPlayerResources();
 }
