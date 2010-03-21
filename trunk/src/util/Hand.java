442d15a4b1938bd6aa4b415eb72c083adfdcc8da
Alex.Kagioglu <Alex.Kagioglu@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-21 22:33:09 +0000

    2010-03-21-18:29 implemented and tested hand.

--- trunk/src/util/Hand.java
+++ trunk/src/util/Hand.java
@@ -5,8 +5,8 @@ package src.util;
  * @author kagioglu
  */
 public interface Hand<T extends HasComparable> {
-    boolean add(T item);
-    boolean remove(T item);
+    T add(T item);
+    T remove(T item);
     Lens<T> spawnLens();
     void clear();
 }
