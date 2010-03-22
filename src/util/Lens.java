15881fc96cc3c7e6de51186446946dec1c657790
Alex.Kagioglu <Alex.Kagioglu@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-22 11:20:58 +0000

    2010-03-22-07:20 added KeyEventInterpreter classes!

--- src/util/Lens.java
+++ src/util/Lens.java
@@ -1,10 +1,13 @@
 package src.util;
 
+import java.util.Collection;
+
 /**
  *
  * @author kagioglu
  */
 public interface Lens<T extends HasComparable> {
+    void fill(Collection<T> coll);
     void next(DataVisitor<T> visitor);
     void prev(DataVisitor<T> visitor);
     void pick(T item, DataVisitor<T> visitor);
