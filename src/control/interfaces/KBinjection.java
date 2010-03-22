6030167ae3db3ba7b7346543fb08d13136991bad
chris.woolfe <chris.woolfe@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-22 16:29:12 +0000

    see KBinjection and DisplayableKeyMap

--- src/control/interfaces/KBinjection.java
+++ src/control/interfaces/KBinjection.java
@@ -3,5 +3,5 @@ import java.util.List;
 import src.control.interfaces.DisplayableBinding;
 
 public interface KBinjection {
-	public void displayControls(List<String> contexts, List<DisplayableBinding> lst, DisplayableBinding selected);
+	public void displayControls(DisplayableKeyMap controls);
 	}
