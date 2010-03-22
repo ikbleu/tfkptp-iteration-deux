9302a37370b2fdcc4acc31c621c2a6e4251aea47
Scandinavianblue <Scandinavianblue@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-22 15:43:51 +0000

    added healing items to view

--- src/view/BobTheMapBuilder.java
+++ src/view/BobTheMapBuilder.java
@@ -52,6 +52,11 @@ class BobTheMapBuilder implements MapBuilder{
 
 
     BobTheMapBuilder(){
+        imageBuffer = new BufferedImage(200, 173,
+                                        BufferedImage.TYPE_INT_ARGB);
+        graphix = imageBuffer.createGraphics();
+        graphix.setRenderingHint( RenderingHints.KEY_ANTIALIASING,
+                                  RenderingHints.VALUE_ANTIALIAS_ON );
         structure = null;
         soldiers = -1;
         resources = null;
@@ -122,11 +127,7 @@ class BobTheMapBuilder implements MapBuilder{
     BufferedImage buildMeViewPort(){
         if(terrain!=null){
             //imageBuffer = (graphicsT.getGraphic(terrain)).clone()
-            imageBuffer = new BufferedImage(200, 173,
-                                        BufferedImage.TYPE_INT_ARGB);
-            graphix = imageBuffer.createGraphics();
-            graphix.setRenderingHint( RenderingHints.KEY_ANTIALIASING,
-                                  RenderingHints.VALUE_ANTIALIAS_ON );
+            
             graphix.drawImage(graphicsT.getGraphic(terrain), 0, 0, null);
             graphix.setColor( Color.BLACK );
             graphix.setFont(f1);
