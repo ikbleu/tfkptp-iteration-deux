2cde4cf919abd315aa4b908e64e774161c618d89
Alex.Kagioglu <Alex.Kagioglu@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-22 20:24:42 +0000

    2010-03-22-11:51 added test configurers

--- src/control/fakemodel/FakeModelConfig.java
+++ src/control/fakemodel/FakeModelConfig.java
@@ -5,18 +5,22 @@ import src.util.Lens;
 import src.util.Hand;
 import src.util.HandFactory;
 import src.util.handv1.HandFactoryImp;
+import java.util.List;
 
 /**
  *
  * @author kagioglu
  */
 public class FakeModelConfig {
-    public static Lens<Device> make(Ousput ous) {
+    public static Lens<Device> make(Ousput ous, List<Device> devis) {
         HandFactory factory = new HandFactoryImp();
-        Hand<Device> handy = factory.make(Device.class);
-        handy.add(new BasicDevices(factory, ous, 4));
-        handy.add(new BasicDevices(factory, ous, 5));
-        handy.add(new BasicDevices(factory, ous, 6));
+        Hand handy = factory.make(Device.class);
+        devis.add(new BasicDevices(factory, ous, 4));
+        handy.add(devis.get(0));
+        devis.add(new BasicDevices(factory, ous, 5));
+        handy.add(devis.get(1));
+        devis.add(new BasicDevices(factory, ous, 6));
+        handy.add(devis.get(2));
         return handy.spawnLens();
     }
 }
