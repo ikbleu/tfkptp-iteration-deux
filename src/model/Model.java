4c25d653ad099a8a6dca0949cd7f2169c14379be
wiredshaiberu <wiredshaiberu@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-22 02:26:48 +0000

    More clockening.

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,5 +1,9 @@
 package src.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import src.model.exceptions.YoureDoingItWrongException;
 import src.model.instances.rallypoints.RallyPointInitializer;
 import src.model.instances.structures.StructureInitializer;
 import src.model.instances.units.UnitInitializer;
@@ -8,11 +12,29 @@ import src.util.HandFactory;
 
 public class Model {
 	public Player p; // TODO: remove (testing purposes)
-	public Clock c;
+	private Clock c;
+	private GameMap map; 
+	
+	public final int TICK_LENGTH = 1000;
 	
 	public Model( HandFactory hFact )
 	{
-		human = new Player( true, hFact, null, null, null, null );
+		List<String> orderList = new ArrayList<String>();
+		orderList.add("Model");
+		orderList.add("VisUpdate");
+		orderList.add("View");
+		
+		try {
+			c = new DethKlok(orderList, TICK_LENGTH);
+		} catch (YoureDoingItWrongException e)
+		{
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
+		map = new GameMap(this);
+		
+		human = new Player( true, hFact, map, map.getStartingLocation1(), null, c);
 		p = human; // TODO: remove
 		// TODO: ai? map?
 		
