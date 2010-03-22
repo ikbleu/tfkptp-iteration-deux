b44a0f7906ab4456423e18f3a30a61ff5b1abec0
ross456 <ross456@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-22 18:12:02 +0000

    added starter unit

---
+++ trunk/src/model/instances/units/StarterUnit.java
@@ -0,0 +1,32 @@
+package src.model.instances.units;
+
+import src.model.Player;
+import src.model.instances.Instance;
+import src.model.instances.Unit;
+import src.model.interfaces.GameTile;
+
+public class StarterUnit extends Unit {
+
+	public StarterUnit(Player p, int id, GameTile g) {
+		super(p, id, g);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String token() {
+		// TODO Auto-generated method stub
+		return "instanceStarter";
+	}
+
+	@Override
+	public void entered(Instance l) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void exited(Instance l) {
+		// TODO Auto-generated method stub
+
+	}
+}
