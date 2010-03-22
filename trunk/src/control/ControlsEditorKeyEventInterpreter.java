edbf1cdc7334a31dde375283131f729057fa6b16
chris.woolfe <chris.woolfe@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-22 16:08:20 +0000

    see KBinjection

---
+++ trunk/src/control/ControlsEditorKeyEventInterpreter.java
@@ -0,0 +1,27 @@
+package src.control;
+
+import src.control.interfaces.InterpretableKeyConfig;
+import src.control.interfaces.KeyEditor;
+import src.control.interfaces.KeyEventInterpreter;
+
+public class ControlsEditorKeyEventInterpreter implements KeyEventInterpreter {
+
+	@Override
+	public void editAncestor(KeyEditor editor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void editDescendant(KeyEditor editor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void interpret(InterpretableKeyConfig ikc) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
