5e4742002876b4820aca12e504a9c52af7143e1c
Alex.Kagioglu <Alex.Kagioglu@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-22 02:12:54 +0000

    2010-03-21-22:11 added interfaces

--- src/control/interfaces/KeyEventInterpreter.java
+++ src/control/interfaces/KeyEventInterpreter.java
@@ -5,5 +5,7 @@ package src.control.interfaces;
  * @author kagioglu
  */
 public interface KeyEventInterpreter {
-
+    void interpret(InterpretableKeyConfig ikc);
+    void editAncestor(KeyEditor editor);
+    void editDescendant(KeyEditor editor);
 }
