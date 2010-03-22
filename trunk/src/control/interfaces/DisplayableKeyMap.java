3629bd0244165660dc9d908f1ecc5a5fe755876d
chris.woolfe <chris.woolfe@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-22 16:29:12 +0000

    see KBinjection and DisplayableKeyMap

---
+++ trunk/src/control/interfaces/DisplayableKeyMap.java
@@ -0,0 +1,26 @@
+package src.control.interfaces;
+
+import java.util.List;
+
+public interface DisplayableKeyMap {
+	/**
+	 * @return the current DisplayableKeyMap context String.
+	 */
+	public String context();
+	
+	/**
+	 * 
+	 * @return true if there is another context. False if none. 
+	 */
+	public boolean hasNext();
+	/**
+	 * changes the current context to the next one.
+	 */
+	public void nextContext();
+	
+	/**
+	 * 
+	 * @return list of DisplayableBindings for a given context. 
+	 */
+	public List<DisplayableBinding> getBindingList();
+}
