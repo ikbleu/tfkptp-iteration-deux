bdf7a0440a822cf3cb8776558eab1832a461a83d
chris.woolfe <chris.woolfe@275bae64-23bb-11df-aa88-49b14aa33a48>
2010-03-22 15:51:22 +0000

    added "selected" to KBinjectino

--- trunk/src/control/interfaces/KBinjection.java
+++ trunk/src/control/interfaces/KBinjection.java
@@ -3,5 +3,5 @@ import java.util.List;
 import src.control.interfaces.DisplayableBinding;
 
 public interface KBinjection {
-	public void displayAContextAndItsBindings(String context, List<DisplayableBinding> lst);
+	public void displayAContextAndItsBindings(String context, List<DisplayableBinding> lst, DisplayableBinding selected);
 }
